a1ec62c1da2726b0a1ea4193f3aaa8fa98f1dd3a
Shpakova Alena <shpakovadev@gmail.com>
2019-09-20 17:45:04 +0300

    gallery html

--- src/app/Event/event.js
+++ src/app/Event/event.js
@@ -1,22 +1,65 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import DeleteIcon from '@material-ui/icons/Delete'
+import IconButton from '@material-ui/core/IconButton'
 
-import Navbar from "../../components/navbar";
+import Navbar from '../../components/navbar'
 
 export default class Event extends Component {
-    render() {
-        return (
-            <div className="page-container">
-                <Navbar/>
-                <div className="section">
-                    Gallery
+  render() {
+    return (
+      <div className="page-container">
+        <Navbar />
+        <div className="content">
+          <h1>Add photos of yours from your sporting life</h1>
+          <div className="section gallery">
+            <header className="gallery__header">
+              <div className="gallery__title">Media</div>
+              <Button variant="outlined" size="medium">
+                create album
+              </Button>
+            </header>
+            <div>
+              <div className="card">
+                <img
+                  className="card__img"
+                  src="https://picsum.photos/300/150"
+                  alt=""
+                />
+                <div className="card__title">Album name</div>
+                <div className="card__actions">
+                  <Button size="small">edit album</Button>
+                  <IconButton aria-label="Delete">
+                    <DeleteIcon fontSize="small" />
+                  </IconButton>
                 </div>
-                <div className="section">
-                    Other sections
+              </div>
+              <div className="card">
+                <img
+                  className="card__img"
+                  src="https://picsum.photos/300/150"
+                  alt=""
+                />
+                <div className="card__title">Album name</div>
+                <div className="card__actions">
+                  <Button size="small">edit album</Button>
+                  <IconButton aria-label="Delete">
+                    <DeleteIcon fontSize="small" />
+                  </IconButton>
                 </div>
-                <div className="section">
-                    Other sections
-                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+          <footer className="actions">
+            <Button variant="outlined" size="medium">
+              I will do it later
+            </Button>
+            <Button variant="contained" size="medium">
+              Finish
+            </Button>
+          </footer>
+        </div>
+      </div>
+    )
+  }
 }
